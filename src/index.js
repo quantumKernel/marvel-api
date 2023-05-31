78b4fbcc91109298c23119abd966fcddb6d0492d
Yevhen Hryshchenko <eugene.grishchenko@live.com>
2023-05-31 16:09:09 +0300

    Modefied Marvell sevice to process and return proper data isolated external api calls interface from main user interface modules add random char info in one block

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-

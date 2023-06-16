f0b27fcae409c148682d29d9555316f128aa5281
Yevhen Hryshchenko <eugene.grishchenko@live.com>
2023-06-16 17:18:43 +0300

    Added fix to show explicitly that loading data about character is in progress

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -27,6 +27,10 @@ class RandomChar extends Component {
         this.setState({character, loading: false});
     }
 
+    onCharLoading = () => {
+        this.setState({loading: true});
+    }
+
     onError = () => {
         this.setState({
             loading: false,
@@ -36,6 +40,7 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacters(id)
             .then(this.onCharLoaded)
